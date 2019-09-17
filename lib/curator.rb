c67ab99af92485ff54cc6ec268eb9790dbd0242d
Ryan Hantak <rhantak@gmail.com>
2019-09-17 10:09:57 -0600

    Add find_photographs_by_artist, artists_with_multiple_photographs, and photographs_taken_by_artists_from to Curator

--- lib/curator.rb
+++ lib/curator.rb
@@ -21,4 +21,33 @@ class Curator
   def find_photograph_by_id(photo_id)
     @photographs.find{|photograph| photograph.id == photo_id}
   end
+
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photograph|
+      photograph.artist_id == artist.id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    artist_ids = @photographs.map do |photograph|
+      photograph.artist_id
+    end
+    have_a_photo = []
+    multiple_photos = []
+    artist_ids.each do |artist_id|
+      multiple_photos << artist_id if have_a_photo.include?(artist_id)
+      have_a_photo << artist_id if !have_a_photo.include?(artist_id)
+    end
+    artists = []
+    multiple_photos.each do |artist_id|
+      artists << find_artist_by_id(artist_id)
+    end
+    artists
+  end
+
+  def photographs_taken_by_artists_from(country)
+    @photographs.find_all do |photograph|
+      find_artist_by_id(photograph.artist_id).country == country
+    end
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -8,16 +8,28 @@ class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
     @photo_1 = Photograph.new({
-       id: "1",
-       name: "Rue Mouffetard, Paris (Boy with Bottles)",
-       artist_id: "1",
-       year: "1954"
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
     })
     @photo_2 = Photograph.new({
-       id: "2",
-       name: "Moonrise, Hernandez",
-       artist_id: "2",
-       year: "1941"
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    })
+    @photo_3 = Photograph.new({
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    })
+    @photo_4 = Photograph.new({
+      id: "4",
+      name: "Monolith, The Face of Half Dome",
+      artist_id: "3",
+      year: "1927"
     })
     @artist_1 = Artist.new({
       id: "1",
@@ -31,6 +43,13 @@ class CuratorTest < Minitest::Test
       name: "Ansel Adams",
       born: "1902",
       died: "1984",
+      country: "United States"
+    })
+    @artist_3 = Artist.new({
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
       country: "United States"
     })
   end
@@ -74,5 +93,42 @@ class CuratorTest < Minitest::Test
 
     assert_equal @photo_1, @curator.find_photograph_by_id("1")
     assert_equal @photo_2, @curator.find_photograph_by_id("2")
+  end
+
+  def test_it_can_find_photos_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@photo_3, @photo_4], @curator.find_photographs_by_artist(@artist_3)
+  end
+
+  def test_it_can_find_artists_with_multiple_photographs
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@artist_3], @curator.artists_with_multiple_photographs
+  end
+
+  def test_it_can_find_photos_taken_by_artists_from_a_certain_country
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@photo_2, @photo_3, @photo_4], @curator.photographs_taken_by_artists_from("United States")
+    assert_equal [], @curator.photographs_taken_by_artists_from("Argentina")
   end
 end
